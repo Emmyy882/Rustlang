af209b513e1b3c3f353b9626b29e892e6d7863dc
Emmyy882 <emmaagbaje3@gmail.com>
2024-06-26 11:12:39 +0100

    guessing_game

---
+++ guessing_game/target/debug/deps/guessing_game-712820b6f02e3236.d
@@ -0,0 +1,5 @@
+/home/emmyy/Rustlang/guessing_game/target/debug/deps/guessing_game-712820b6f02e3236: src/main.rs
+
+/home/emmyy/Rustlang/guessing_game/target/debug/deps/guessing_game-712820b6f02e3236.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/emmyy/Rustlang/guessing_game/target/debug/guessing_game: /home/emmyy/Rustlang/guessing_game/src/main.rs
