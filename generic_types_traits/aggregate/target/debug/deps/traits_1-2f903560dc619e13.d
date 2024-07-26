487c46711e7ca6acdab590060fc1ae16326a2bab
Emmyy882 <emmaagbaje3@gmail.com>
2024-07-26 16:05:02 +0100

    exploring traits usage

---
+++ generic_types_traits/aggregate/target/debug/deps/traits_1-2f903560dc619e13.d
@@ -0,0 +1,5 @@
+/home/emmyy/Rustlang/generic_types_traits/aggregate/target/debug/deps/traits_1-2f903560dc619e13: src/main.rs
+
+/home/emmyy/Rustlang/generic_types_traits/aggregate/target/debug/deps/traits_1-2f903560dc619e13.d: src/main.rs
+
+src/main.rs:
