f693ad7ca4b1ceb4f20b51b4d453c0357f118945
Emmyy882 <emmaagbaje3@gmail.com>
2024-07-30 23:01:51 +0100

    added tests that incoporated integration testing

---
+++ integration_testing/adder/target/debug/deps/integration_test-6ee6374373a16c2a.d
@@ -0,0 +1,5 @@
+/home/emmyy/Rustlang/integration_testing/adder/target/debug/deps/integration_test-6ee6374373a16c2a: tests/integration_test.rs
+
+/home/emmyy/Rustlang/integration_testing/adder/target/debug/deps/integration_test-6ee6374373a16c2a.d: tests/integration_test.rs
+
+tests/integration_test.rs:
