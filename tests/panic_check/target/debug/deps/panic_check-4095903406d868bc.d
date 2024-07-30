13f031ec4db6ea61a8963358ce2e6ace32595d46
Emmyy882 <emmaagbaje3@gmail.com>
2024-07-30 16:28:53 +0100

    exploring test with example

---
+++ tests/panic_check/target/debug/deps/panic_check-4095903406d868bc.d
@@ -0,0 +1,5 @@
+/home/emmyy/Rustlang/tests/panic_check/target/debug/deps/panic_check-4095903406d868bc: src/lib.rs
+
+/home/emmyy/Rustlang/tests/panic_check/target/debug/deps/panic_check-4095903406d868bc.d: src/lib.rs
+
+src/lib.rs:
