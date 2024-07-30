be0b578e4dc74d26c64c1fd9652a472ba7ff0b8d
Emmyy882 <emmaagbaje3@gmail.com>
2024-07-30 20:31:42 +0100

    testing a subset of a test

---
+++ tests/selective_check/target/debug/deps/selective_check-2e1c5f5fca0fb86a.d
@@ -0,0 +1,5 @@
+/home/emmyy/Rustlang/tests/selective_check/target/debug/deps/selective_check-2e1c5f5fca0fb86a: src/lib.rs
+
+/home/emmyy/Rustlang/tests/selective_check/target/debug/deps/selective_check-2e1c5f5fca0fb86a.d: src/lib.rs
+
+src/lib.rs:
