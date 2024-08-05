7f310dd6655ef059a5740cca7268038c4e4703a1
Emmyy882 <emmaagbaje3@gmail.com>
2024-08-05 12:14:24 +0100

    fixed bugs, modified files and added new implementations

--- mini_grep/src/main.rs
+++ mini_grep/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     /* dbg!(&args); // printing the passed arguments */
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -18,7 +18,7 @@ fn main() {
     println!("In file {}", config.file_path);
 
     if let Err(e) = mini_grep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
