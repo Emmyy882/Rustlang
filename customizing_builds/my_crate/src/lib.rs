d20fd6ef7569ece7099f2e0ffef6f5439ace1d8a
Emmyy882 <emmaagbaje3@gmail.com>
2024-08-07 23:26:23 +0100

    exploring Crate publishing with example

---
+++ customizing_builds/my_crate/src/lib.rs
@@ -0,0 +1,13 @@
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = my_crate::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(num: i32) -> i32 {
+    1 + num
+}
