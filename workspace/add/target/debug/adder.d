531740fe52f7de8ccf72d263545c053ec42ab7cd
Agbaje Itafe Emmanuel <emmaagbaje3@gmail.com>
2024-09-06 13:27:19 +0100

    added '.gitignore' file

--- workspace/add/target/debug/adder.d
+++
@@ -1,1 +0,0 @@
-/home/emmyy/Rustlang/workspace/add/target/debug/adder: /home/emmyy/Rustlang/workspace/add/add_one/src/lib.rs /home/emmyy/Rustlang/workspace/add/adder/src/main.rs
--- workspace/add/target/debug/deps/adder-7d36d50579ac6a18.d
+++
@@ -1,5 +0,0 @@
-/home/emmyy/Rustlang/workspace/add/target/debug/deps/adder-7d36d50579ac6a18: adder/src/main.rs
-
-/home/emmyy/Rustlang/workspace/add/target/debug/deps/adder-7d36d50579ac6a18.d: adder/src/main.rs
-
-adder/src/main.rs:
